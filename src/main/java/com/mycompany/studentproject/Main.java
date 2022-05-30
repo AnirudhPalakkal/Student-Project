c5ef85f0410764197243d75f25a0d0aacbff604f
AnirudhPalakkal <palakkal@sheridancollege.ca>
2022-05-30 17:59:07 -0400

    First commit

---
+++ src/main/java/com/mycompany/studentproject/Main.java
@@ -0,0 +1,27 @@
+package com.mycompany.studentproject;
+/*
+Build an algorithm within a main method that will take
+in a word, storing each character in the word in an
+Array. Your code should take in the word, store the
+word in an array and then iterate backwards over the
+array to print the word in reverse.
+*/
+
+/**
+ *
+ * @author aniru
+ */
+public class Main {
+    public static void main(String [] args) {
+        Student[] studentArr = new Student[5];
+        studentArr[0] = new Student("Liam");
+        studentArr[1] = new Student("Achilles");
+        studentArr[2] = new Student("Georgia");
+        studentArr[3] = new Student("Lucy");
+        studentArr[4] = new Student("Nate");
+        
+        for(Student each : studentArr) {
+            System.out.println(each.getName());
+        }
+    }
+}
---
+++ src/main/java/com/mycompany/studentproject/Student.java
@@ -0,0 +1,21 @@
+package com.mycompany.studentproject;
+
+/**
+ *
+ * @author aniru
+ */
+public class Student {
+    String name;
+    
+    Student(String name) {
+        this.name = name;
+    }
+    
+    String getName() {
+        return this.name;
+    }
+    
+    void setName(String name) {
+        this.name = name;
+    }
+}
